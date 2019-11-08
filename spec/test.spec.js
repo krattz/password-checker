a945436b67767bfa75b842ea2502f3b1acb0f85d
krattz <nthebep@yahoo.com>
2019-11-08 09:39:17 +0200

    spec

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,7 +1,7 @@
 const password = require('../src/password.js');
 
-let pass = 'KillJ0!!2';
-let validate = password.passwordIsValid(pass);
+let psw = 'KillJ0!!2';
+let validate = password.passwordIsValid(psw);
 
 describe("Password checker",function(){
     
@@ -9,27 +9,26 @@ describe("Password checker",function(){
         expect(validate).not.toBeNull();
     });
     it("Should check password length is not less than 8 characters", () => {
-        expect(pass.length).toBeGreaterThan(7);
+        expect(psw.length).toBeGreaterThan(7);
     });
     it("Should check if password has atleast 1 lowercase letter", () => {
         expect(validate).toMatch(/^(?=.*[a-z])/);
     });
-    // it("Should check if password has atleast 1 capital letter", () => {
-    //     expect(password.passwordIsValid(pass)).toMatch(/^(?=.*[A-Z])/);
-    // });
-    // it("Should check if password has atleast 1 number", () => {
-    //     expect(validate).toMatch(/^(?=.*\d)/);
-    // });
-    it("Should check password for atleast 1 special character",()=>{
-        expect(password.passwordIsOk(pass)).toMatch(/[ \ ^ $ . ! @ % & | ? * + { } ( ) + = - _  ; :]/g);
+    it("Should check if password has atleast 1 capital letter", () => {
+        expect(psw).toMatch(/^(?=.*[A-Z])/);
     });
-    
+    it("Should check if password has atleast 1 number", () => {
+        expect(psw).toMatch(/^(?=.*\d)/);
+    });
+    it("Should check password for atleast 1 special character",()=>{
+        expect(password.passwordIsOk(psw)).toMatch(/[ \ ^ $ . ! @ % & | ? * + { } ( ) + = - _  ; :]/g);
+    });
 });
 
 describe("Password verify",function(){
     
     it("Should Check if password is OK",()=>{
-        expect(password.passwordIsOk(pass)).toBe('Password is okay');
+        expect(password.passwordIsOk(psw)).toBe('Password is okay');
     });
 });
 
